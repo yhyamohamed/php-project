4cc7897a441295445272de813ae61757b92ca4ed
yhyamohamed <62445980+yhyamohamed@users.noreply.github.com>
2022-03-09 08:03:20 +0200

    setting up user controller / model

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -1,23 +1,26 @@
 <?php
+
+namespace App\Controllers;
+
 interface Controller
 {
-    public function index();//list all resource data 
-    public function create();//render form for data
-    public function store(User $user);//presist new resource data (from request) & redirect 
-    public function show($id);
-    public function edit($id);//render data in form to edit 
-    public function update($id);//presist  edits & redirect 
+    public function index(); //list all resource data 
+    public function create(); //render form for data
+    public function store($user); //presist new resource data (from request) & redirect 
+    public function show($username,$pass);
+    public function edit($id); //render data in form to edit 
+    public function update($id); //presist  edits & redirect 
     public function destroy($id);
 
 
 
-// Verb	        URI	                Action	    Route-Name
-// ==============================================================
-// GET	        /photos	            index	    photos.index
-// GET	        /photos/            create	    create	photos.create
-// POST	        /photos	            store	    photos.store
-// GET	        /photos/{photo} 	show	    photos.show
-// GET	        /photos/{photo}/    edit	    edit	photos.edit
-// PUT/PATCH	/photos/{photo}	    update	    photos.update
-// DELETE	    /photos/{photo}	    destroy	    photos.destroy
+    // Verb	        URI	                Action	    Route-Name
+    // ==============================================================
+    // GET	        /photos	            index	    photos.index
+    // GET	        /photos/            create	    create	photos.create
+    // POST	        /photos	            store	    photos.store
+    // GET	        /photos/{photo} 	show	    photos.show
+    // GET	        /photos/{photo}/    edit	    edit	photos.edit
+    // PUT/PATCH	/photos/{photo}	    update	    photos.update
+    // DELETE	    /photos/{photo}	    destroy	    photos.destroy
 }
--- App/Model/UserTest.php
+++ App/Model/UserTest.php
@@ -1,10 +1,12 @@
 <?php
+namespace App\Model;
+
 interface UserTest
 {
     // note: 1- implement setter for database fields
     
     // 2-functions 
-    public function login(); 
+    public function login($email,$hashedPass); 
     public function logout(); 
     public function register(); 
     public function update($id);
